9897e81dca3ed0c864f8976d0e505d0f4d1b064c
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-01-21 19:17:58 +0530

    initial commit

---
+++ NodeJS_CodeStepByStep/index.js
@@ -0,0 +1,22 @@
+// var a =20;
+// var b =30;
+// var c =40;
+// console.log(a+b+c);
+
+const app = require('./app1')
+// console.log(app);
+// console.log(app.x,app.y);
+
+const arr = [2, 4, 7, 1, 3, 8, 3]
+let result = arr.filter((el) => {
+    return el > 3;
+})
+console.warn(result);
+
+// var x = '20';
+// let y = x === 20 ? console.log('Matched') : console.log('Mismatched')
+// console.log(y);
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
