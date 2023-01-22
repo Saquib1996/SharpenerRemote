217b0fda2242a506a3d2a2a7d637eba5f4ed9e8d
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-01-22 09:54:28 +0530

    initial commit

---
+++ NodeJS_CodeStepByStep/data.js
@@ -0,0 +1,7 @@
+const data = [
+    { name: 'Saquib', email: 'saquib@gmail.com' },
+    { name: 'Aquib', email: 'aquib@gmail.com' },
+    { name: 'Raghib', email: 'raghib@gmail.com' },
+    { name: 'Hasina', email: 'hasina@gmail.com' }
+]
+module.exports = data;
