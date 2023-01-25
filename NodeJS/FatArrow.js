36c2559b6bdcd1467753c134172328994b77b982
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-01-25 19:24:04 +0530

    initial commit

---
+++ NodeJS/FatArrow.js
@@ -0,0 +1,13 @@
+// const product = (a,b) => a*b;
+// console.log(product(4,5));
+
+const Student = {
+    Name: 'Pablo Kumar',
+    Age: 26,
+    Class: 12,
+    Subjects: ['Physics', 'Chemistry', 'Math', 'Urdu', 'English'],
+    RollNo: 1507745,
+    MobNo: 9471225996
+}
+
+console.log(Student);
