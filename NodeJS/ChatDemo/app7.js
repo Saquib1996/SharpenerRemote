b0d5f5f17fd5828f37be9027547faed2b3d70176
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-01-26 18:56:42 +0530

    ChattingApp

---
+++ NodeJS/ChatDemo/app7.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const body=[];
+    const method = req.method;
+    if (url === '/') {
+        fs.readFile('message.txt', { encoding: 'utf-8' }, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            res.write('<html>');
+            res.write('<head><title>Enter Message</title><head>');
+            res.write(`<body>${data}</body>`);
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        });
+    } else if (url === '/message' && method === 'POST') {
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', function () {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1]; //['message','My name is Saquib']
+            fs.writeFile('message.txt', message, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    } 
+    else {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title><head>');
+        res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+        res.write('<html>');
+        res.end();
+
+    }
+});
+server.listen(4500);
