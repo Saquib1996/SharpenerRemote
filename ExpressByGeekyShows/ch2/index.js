da78cfd2e7a6fa2a389760dd5cfe125bd36b0757
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-03-10 21:55:45 +0530

    First commit

---
+++ ExpressByGeekyShows/ch2/index.js
@@ -0,0 +1,15 @@
+// Setup Babel with express js
+
+// const express = require('express')
+
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Sonam')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
