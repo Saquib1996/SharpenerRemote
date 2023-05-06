4d3a9585dbb32723394a4f98daa6be4b83dac634
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-05-06 18:35:34 +0530

    Second commit

---
+++ Udemy/ch65/app.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/', (req, res, next) => {
+    // console.log('This always runs');
+    next();
+})
+
+app.use('/add-product', (req, res, next) => {
+    // console.log('In another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+app.use('/add-size', (req, res, next) => {
+    // console.log('In another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="Size"><button type="submit">Enter Size</button></form>');
+});
+app.get('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    // console.log('In another middleware');
+    res.send('<h1>Hello from express</h1>');
+});
+
+// const server = http.createServer(app);
+// server.listen(3000)
+// Or
+
+app.listen(3010);
