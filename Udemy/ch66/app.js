db91d476b3743962ed59149afbf98c925d278636
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-05-06 11:56:23 +0530

    Using Express Router

---
+++ Udemy/ch66/app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const express = require('express');
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(shopRoutes);
+app.use(adminRoutes);
+
+
+
+app.use('/add-size', (req, res, next) => {
+    // console.log('In another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="Size"><button type="submit">Enter Size</button></form>');
+});
+
+
+
+// const server = http.createServer(app);
+// server.listen(3000)
+// Or
+
+app.listen(3010);
