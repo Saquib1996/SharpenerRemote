81d4ad33f4ffcf4d9c4cf4978305d88c06caaae9
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-07-05 16:44:43 +0530

    second commit

--- Udemy/ch70/app.js
+++ Udemy/ch70/app.js
@@ -1,3 +1,6 @@
+const path = require('path')
+
+
 const http = require('http');
 const express = require('express');
 
@@ -12,7 +15,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next) => {
-    res.status(404).send('<h1> Page not found </h1>')
+    res.status(404).sendFile('<h1> Page not found </h1>')
 })
 
 
