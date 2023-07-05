0290150fa672f733b074e5b4f05883b463ff4068
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-07-05 16:50:29 +0530

    iecond commit

---
+++ SimpleiLearn/ReactComp/rcomp/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import logo from './logo.svg';
+// import './App.css';
+import FC from "./Components/FunctionalComp";
+import ClassComp from "./Components/ClassComp";
+function App() {
+  return (
+  <div>
+    <h1>Hello! Welcome to SimpleiLearn</h1>
+    <h1>This video is about Components</h1>
+    <FC/>
+    <ClassComp/>
+  </div>
+    );
+}
+
+export default App;
---
+++ SimpleiLearn/ReactComp/rcomp/src/Components/ClassComp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ClassComp extends React.Component{
+    render(){
+        return <p>This is the Class Components</p>
+    }
+}
+
+export default ClassComp;
