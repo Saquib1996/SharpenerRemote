5691b9bd9620b730f13539032b8d9232078397b3
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-07-11 21:29:49 +0530

    Second commit

--- ExpressByUdemy/StylingOurPages/app.js
+++ ExpressByUdemy/StylingOurPages/app.js
@@ -8,6 +8,8 @@ const app = express();
 
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+const contactRoutes = require('./routes/contact.js');
+const successRoutes = require('./routes/success.js')
 
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -16,7 +18,8 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
-
+app.use(contactRoutes);
+app.use(successRoutes);
 app.use((req,res,next) => {
     res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
