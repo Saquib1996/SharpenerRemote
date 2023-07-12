88f94a8285415c772ce12ae6c1bb9636dd4b45cb
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-07-12 18:15:43 +0530

    Model_View_Controller

---
+++ ExpressByUdemy/MVC/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const path = require('path');
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+const contactRoutes = require('./routes/contact.js');
+const successRoutes = require('./routes/success.js')
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+})
+
+// const server = http.createServer(app);
+// server.listen(3000)
+
+/***************************------------------OR-------------------*****************************/
+
+app.listen(3000, () => {
+    console.log('Server is running at Port 3000');
+});
